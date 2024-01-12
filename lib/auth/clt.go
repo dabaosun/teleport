187878128773572daac7b6b47b98f968e40ea7aa
joerger <bjoerger@goteleport.com>
2024-01-11 18:27:02 -0800

    Refactor MFA prompt logic for the API client to enable various MFA ceremony use cases.

--- lib/auth/clt.go
+++ lib/auth/clt.go
@@ -40,6 +40,7 @@ import (
 	resourceusagepb "github.com/gravitational/teleport/api/gen/proto/go/teleport/resourceusage/v1"
 	samlidppb "github.com/gravitational/teleport/api/gen/proto/go/teleport/samlidp/v1"
 	userpreferencesv1 "github.com/gravitational/teleport/api/gen/proto/go/userpreferences/v1"
+	"github.com/gravitational/teleport/api/mfa"
 	"github.com/gravitational/teleport/api/types"
 	apievents "github.com/gravitational/teleport/api/types/events"
 	accessgraphv1 "github.com/gravitational/teleport/gen/proto/go/accessgraph/v1alpha"
@@ -992,4 +993,7 @@ type ClientI interface {
 	// ValidateMFAAuthResponse validates an MFA or passwordless challenge.
 	// Returns the device used to solve the challenge (if applicable) and the username.
 	ValidateMFAAuthResponse(ctx context.Context, resp *proto.MFAAuthenticateResponse, user string, passwordless bool) (*types.MFADevice, string, error)
+
+	// PromptMFA prompts the user for MFA.
+	PromptMFA(ctx context.Context, chal *proto.MFAAuthenticateChallenge, promptOpts ...mfa.PromptOpt) (*proto.MFAAuthenticateResponse, error)
 }
